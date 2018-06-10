ea5713e4be7a64e8a329bd4cc85f17f76477980a
ccremer <github.account@chrigel.net>
2018-06-10 14:12:39 +0200

    Add config & event subprojects

---
+++ clustercode.api.config/src/main/java/clustercode/api/config/PathConverter.java
@@ -0,0 +1,15 @@
+package clustercode.api.config;
+
+import clustercode.impl.util.FilesystemProvider;
+import org.aeonbits.owner.Converter;
+
+import java.lang.reflect.Method;
+import java.nio.file.Path;
+
+public class PathConverter implements Converter<Path> {
+
+    @Override
+    public Path convert(Method method, String input) {
+        return FilesystemProvider.getInstance().getPath(input);
+    }
+}
