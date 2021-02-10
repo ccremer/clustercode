3d5dfa1f9baffb8bd19d840104efdc3533536d40
Chris <github.account@chrigel.net>
2021-02-10 22:23:51 +0100

    Generate API reference for Antora

--- generate.go
+++ generate.go
@@ -5,7 +5,11 @@ package main
 //go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile="kustomize/boilerplate.go.txt" paths="./..."
 //go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen crd:trivialVersions=true rbac:roleName=manager-role webhook paths="./..." output:crd:artifacts:config=${CRD_ROOT_DIR}/v1alpha1 crd:crdVersions=v1 output:dir=kustomize
 
+// Generate API reference documentation
+//go:generate go run github.com/elastic/crd-ref-docs --source-path=api/v1alpha1 --config=docs/api-gen-config.yaml --renderer=asciidoctor --templates-dir=docs/api-templates --output-path=${CRD_DOCS_REF_PATH}
+
 import (
+	_ "github.com/elastic/crd-ref-docs"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kustomize/kustomize/v3"
 )
