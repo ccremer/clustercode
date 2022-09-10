1d7cf36715b98f6f599f37a2051d5fbcd815955a
Chris <github.account@chrigel.net>
2022-09-10 15:00:20 +0200

    Fix wrong webhook pathing

--- pkg/webhook/blueprintwebhook/setup.go
+++ pkg/webhook/blueprintwebhook/setup.go
@@ -5,8 +5,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
-// +kubebuilder:webhook:verbs=create;update,path=/validate-clustercode-github-io-v1alpha1-blueprint,mutating=true,failurePolicy=fail,groups=clustercode.github.io,resources=blueprints,versions=v1alpha1,name=blueprints.clustercode.github.io,sideEffects=None,admissionReviewVersions=v1
-// +kubebuilder:webhook:verbs=create;update,path=/mutate-clustercode-github-io-v1alpha1-blueprint,mutating=false,failurePolicy=fail,groups=clustercode.github.io,resources=blueprints,versions=v1alpha1,name=blueprints.clustercode.github.io,sideEffects=None,admissionReviewVersions=v1
+// +kubebuilder:webhook:verbs=create;update,path=/validate-clustercode-github-io-v1alpha1-blueprint,mutating=false,failurePolicy=fail,groups=clustercode.github.io,resources=blueprints,versions=v1alpha1,name=blueprints.clustercode.github.io,sideEffects=None,admissionReviewVersions=v1
+// +kubebuilder:webhook:verbs=create;update,path=/mutate-clustercode-github-io-v1alpha1-blueprint,mutating=true,failurePolicy=fail,groups=clustercode.github.io,resources=blueprints,versions=v1alpha1,name=blueprints.clustercode.github.io,sideEffects=None,admissionReviewVersions=v1
 
 // SetupWebhook adds a webhook for v1alpha1.Blueprint managed resources.
 func SetupWebhook(mgr ctrl.Manager) error {
