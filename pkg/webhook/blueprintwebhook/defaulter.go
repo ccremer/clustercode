db141338f7800fc3884f8e1ce8419c7fdce25868
Chris <github.account@chrigel.net>
2022-09-10 14:06:38 +0200

    Add defaulter mutating webhook

--- pkg/webhook/blueprintwebhook/defaulter.go
+++ pkg/webhook/blueprintwebhook/defaulter.go
@@ -3,13 +3,43 @@ package blueprintwebhook
 import (
 	"context"
 
+	"github.com/ccremer/clustercode/pkg/api/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-type Defaulter struct {
-}
+type Defaulter struct{}
 
-func (d *Defaulter) Default(ctx context.Context, obj runtime.Object) error {
-	// TODO implement me
-	panic("implement me")
+func (d *Defaulter) Default(_ context.Context, obj runtime.Object) error {
+	bp := obj.(*v1alpha1.Blueprint)
+
+	if bp.Spec.MaxParallelTasks == 0 {
+		bp.Spec.MaxParallelTasks = 1
+	}
+
+	if bp.Spec.Encode.SliceSize == 0 {
+		bp.Spec.Encode.SliceSize = 120
+	}
+
+	if len(bp.Spec.Scan.MediaFileExtensions) == 0 {
+		bp.Spec.Scan.MediaFileExtensions = []string{"mkv", "mp4", "avi"}
+	}
+
+	if len(bp.Spec.Encode.SplitCommandArgs) == 0 {
+		bp.Spec.Encode.SplitCommandArgs = []string{
+			"-y", "-hide_banner", "-nostats", "-i", "${INPUT}", "-c", "copy", "-map", "0", "-segment_time", "${SLICE_SIZE}", "-f", "segment", "${OUTPUT}",
+		}
+	}
+
+	if len(bp.Spec.Encode.TranscodeCommandArgs) == 0 {
+		bp.Spec.Encode.TranscodeCommandArgs = []string{
+			"-y", "-hide_banner", "-nostats", "-i", "${INPUT}", "-c:v", "copy", "-c:a", "copy", "${OUTPUT}",
+		}
+	}
+
+	if len(bp.Spec.Encode.MergeCommandArgs) == 0 {
+		bp.Spec.Encode.MergeCommandArgs = []string{
+			"-y", "-hide_banner", "-nostats", "-f", "concat", "-i", "concat.txt", "-c", "copy", "media_out.mkv",
+		}
+	}
+	return nil
 }
---
+++ pkg/webhook/blueprintwebhook/defaulter_test.go
@@ -0,0 +1,38 @@
+package blueprintwebhook
+
+import (
+	"testing"
+
+	"github.com/ccremer/clustercode/pkg/api/v1alpha1"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaulter_Default(t *testing.T) {
+	tests := map[string]struct {
+		givenSpec    v1alpha1.BlueprintSpec
+		expectedSpec v1alpha1.BlueprintSpec
+	}{
+		"GivenEmptySpec_ThenExpectDefaultsSet": {
+			givenSpec: v1alpha1.BlueprintSpec{},
+			expectedSpec: v1alpha1.BlueprintSpec{
+				Scan:             v1alpha1.ScanSpec{MediaFileExtensions: []string{"mkv", "mp4", "avi"}},
+				MaxParallelTasks: 1,
+				Encode: v1alpha1.EncodeSpec{
+					SliceSize:            120,
+					SplitCommandArgs:     []string{"-y", "-hide_banner", "-nostats", "-i", "${INPUT}", "-c", "copy", "-map", "0", "-segment_time", "${SLICE_SIZE}", "-f", "segment", "${OUTPUT}"},
+					TranscodeCommandArgs: []string{"-y", "-hide_banner", "-nostats", "-i", "${INPUT}", "-c:v", "copy", "-c:a", "copy", "${OUTPUT}"},
+					MergeCommandArgs:     []string{"-y", "-hide_banner", "-nostats", "-f", "concat", "-i", "concat.txt", "-c", "copy", "media_out.mkv"},
+				},
+			},
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			d := &Defaulter{}
+			bp := &v1alpha1.Blueprint{Spec: tt.givenSpec}
+			err := d.Default(nil, bp)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expectedSpec, bp.Spec)
+		})
+	}
+}
