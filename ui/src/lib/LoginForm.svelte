4c892b3d5d10d6b2c3c1825a14ab3ed59d12985a
Chris <github.account@chrigel.net>
2022-09-13 00:47:03 +0200

    Start with bootstrap theme

---
+++ ui/src/lib/LoginForm.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { Button, Label, Form, FormGroup, Input } from "sveltestrap";
+
+  let token = "";
+
+  async function login() {
+    // TODO: implement
+  }
+</script>
+
+<div>
+
+</div>
+<Form on:submit={login}>
+  <FormGroup floating label="Token">
+    <Input placeholder="Token" id="token" type="password" bind:value={token} />
+  </FormGroup>
+  <Button id="btn-submit" color="primary">Submit</Button>
+</Form>
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,8 +1,11 @@
-import './app.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.svelte'
+import { writable } from 'svelte/store'
 
 const app = new App({
   target: document.getElementById('app')
 })
+
+export const authToken = writable('')
 
 export default app
