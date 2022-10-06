fb6bff4a05ce60ed8a537882ec333dedf1dee02f
Chris <github.account@chrigel.net>
2022-10-06 23:48:12 +0200

    Save token to cookie and reuse

--- ui/src/lib/LoginForm.svelte
+++ ui/src/lib/LoginForm.svelte
@@ -1,10 +1,36 @@
 <script lang="ts">
   import { Button, FormGroup, Input, Alert } from "sveltestrap"
   import { Client, RequestError } from "../kube/client"
+  import { ClientStore } from "../stores/ClientStore"
+  import { onMount } from "svelte"
+  import { ServerSettingsStore } from "../stores/SettingsStore"
 
   let token = ""
   let displayError = ""
   let alertVisible = false
+  let cookieMaxAge: number
+
+  ServerSettingsStore.subscribe(settings => {
+    cookieMaxAge = settings.authCookieMaxAge
+  })
+
+  // returns the cookie with the given name, or undefined if not found
+  function getCookie(name: string): string {
+    // https://javascript.info/cookie
+    const matches = document.cookie.match(
+      new RegExp(`(?:^|; )${name.replace(/([.$?*|{}()[\]\\/+^])/g, "\\$1")}=([^;]*)`)
+    )
+    return matches ? decodeURIComponent(matches[1]) : undefined
+  }
+
+  onMount(() => {
+    const cookie = getCookie("kubetoken")
+    if (cookie) {
+      console.log("retrieved token from cookie, logging in...")
+      token = cookie
+      login()
+    }
+  })
 
   function login() {
     let client = new Client()
@@ -12,11 +38,14 @@
       .login(token)
       .then(ssar => {
         if (ssar.status.allowed) {
+          console.log("successfully logged in")
           dismissError()
-        } else {
-          showError("You are not allowed to view blueprints.")
+          ClientStore.set(client)
+          const secure = location.protocol === "https:" ? "secure" : ""
+          document.cookie = `kubetoken=${token}; max-age=${cookieMaxAge}; samesite=strict; ${secure}`
           return
         }
+        showError("You are not allowed to view blueprints.")
       })
       .catch(err => {
         console.log("cannot login", err)
